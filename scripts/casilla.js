f9ffe4dee6a9d3551624a3aa06571f5ff4166085
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2021-12-27 16:08:07 +0100

    commit de pruebas pero sigue sin funcionar el avatar

---
+++ scripts/casilla.js
@@ -0,0 +1,25 @@
+// let objCasillas =[
+//     {
+//         id : 1,
+//         avatar : "https://ibb.co/nnLrvCJ"
+//     }
+// ]
+
+window.addEventListener("DOMContentLoaded", function () {//todo lo que debe esperar a que se cargue la pagina se mete en esta función
+
+    let jugando = true;
+    let jugadorActual = "";
+    let mensajeGanador = "El "++"ha ganado";
+    let jug1 = "jugador 1";
+    let jug2 = "jugador 2";
+    
+    function mostrarAvatar(){
+        let avatar = 
+    }
+
+
+
+});
+
+// https://ibb.co/QvP3k9c
+//https://ibb.co/nnLrvCJ
