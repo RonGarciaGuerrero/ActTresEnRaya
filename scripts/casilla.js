ad219b8909423a6395bcfa6c1fb2859d8f17dba4
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2021-12-28 18:26:29 +0100

    se modifica el menu de temática, se implementan objetos ranking, jugador, y templates que es un objetos de objetos

--- scripts/casilla.js
+++ scripts/casilla.js
@@ -6,12 +6,58 @@
 // ]
 
 window.addEventListener("DOMContentLoaded", function () {//todo lo que debe esperar a que se cargue la pagina se mete en esta función
+    //Entorno:
+    let jugando = true;
+    
+    let template;//aqui va la tematica escogida con los avatares 
+    let turno = '';
+    const ranking = {
+        partidasJ1: 0,
+        partidasJ2: 0,
+        partidasEmpate:0,
+        totalPartidas: 0    
+    }
 
-    // let jugando = true;
-    // let jugadorActual = "";
-    // //let mensajeGanador = "El "++"ha ganado";
-    // let jug1 = "jugador 1";
-    // let jug2 = "jugador 2";
+    const jugador1 = {
+        nombre: '',
+        avatar: ''
+    }
+
+    const jugador2 = {
+        nombre: '',
+        avatar: ''
+    }
+
+    const templates = {
+        clasico: {
+            avatar1: './img/circulo.gif',
+            avatar2: './img/equis.gif',
+            background: './img/equis.gif'
+        },
+        matrix: {
+            avatar1: './img/neo.png',
+            avatar2: './img/trinity.png',
+            background: './img/trinity.png'
+        }
+    }
+
+
+    const templatesarray = [
+        {
+            avatar1: './img/circulo.gif',
+            avatar2: './img/equis.gif',
+            background: 'cyan'
+        },
+        {
+            avatar1: './img/neo.png',
+            avatar2: './img/trinity.png',
+            background: 'gray'
+        }
+    ]
+
+    //Funcion comprobación
+
+    
     
     //esto no funciona
     // function asignarAvatar(){
@@ -23,14 +69,82 @@ window.addEventListener("DOMContentLoaded", function () {//todo lo que debe espe
     //con el evento change muestro el avatar cogido en el select
     const selectElement = document.querySelector('#selectAvatar1');
     selectElement.addEventListener('change', (event) => {
-        const jugador = document.querySelector('#jugador1');
-        jugador.innerHTML = '<img src="'+`${event.target.value}`+'" height="80">';
+        //const jugador = document.querySelector('#jugador1');
+        //jugador.innerHTML = '<img src="'+`${event.target.value}`+'" height="80">';//${} string template no se necesita usar el + para concatenar
+
+        const selectedTemplate = event.target.value; //valor seleccionado por el usuario cuando cambio la opcion
+
+        const templateInfo = templates[selectedTemplate];
+
+        //actualiza info de jugadores
+        jugador1.avatar = templateInfo.avatar1;
+        jugador2.avatar = templateInfo.avatar2;
+
+        //Acturalizar imagen que se muestra en la configuracion
+        const jugadorDiv1 = document.querySelector('#jugador1');
+        jugadorDiv1.innerHTML = `<img src="${jugador1.avatar}" height="80">`;
+
+        const jugadorDiv2 = document.querySelector('#jugador2');
+        jugadorDiv2.innerHTML = `<img src="${jugador2.avatar}" height="80">`;
+
+        //actualizar el background
+        document.querySelector('body').style.backgroundColor = templateInfo.background;
+
     });
-    const selectElement2 = document.querySelector('#selectAvatar2');
-    selectElement2.addEventListener('change', (event) => {
-        const jugador = document.querySelector('#jugador2');
-        jugador.innerHTML = '<img src="'+`${event.target.value}`+'" height="80">';
-    });
+    
+    document.querySelector('#iniciarPartido').addEventListener('click', function() {
+
+        //validar que se haya seleccionado un template
+        //validar que los nombres se hayan colocado
+        // asignar los nombres a los jugadores
+        //desaparecer la seccion de configuracion
+        // aparecer el tablero y asignar el turno inicial
+    })
+
+
+
+    /*
+    Cuando un jugador hace click en una casilla
+    1) valido si no se ha jugado ya
+    2) Cambio el fondo de la casill apor el avatar de ese jugador
+    3) a partir dle tercer click compruebo si ya se ha ganado o no    
+    4) Si ha ganado o empatado, finalizo juego
+    5) Si no ha ganado o empatado, cambio de turno
+    */
+
+    /**
+     * Finalizar juego
+     * 1) actualizar ranking
+     * 2) Mostrar opcion de revancha o inicio
+     * 3) si el usuario hace click en revancha --> Reinicar partida
+     * 4) si no, volvemos a la pagina inicial de configuracion de usuarios y tablero oculto
+     */
+
+    /**
+     * Reiniciar partida
+     * 1) limpiar casillas
+     * 2) reiniciar turno
+     */
+
+
+
+
+
+
+    
+    var casilla = document.querySelector('#c1');
+    casilla.onclick = asignarAvatar;
+
+
+    function asignarAvatar(){
+        const selectCasilla = document.querySelector('#c1');
+        selectCasilla.addEventListener('click', function(){
+            var imagen = document.get
+            selectCasilla.style.backgroundImage = turno.avatar;
+        });
+        //falta poenr el tamaño y asociar a que sea la seleccionada
+    }
+
 
 });
 
